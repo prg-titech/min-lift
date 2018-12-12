115294304d08e6d9040ccf99e6e36c3eb1f39767
long-long-float <niinikazuki@yahoo.co.jp>
2018-12-13 05:47:27 +0900

    Fix CodeGenerator

--- src/main/scala/pass/CodeGenerator.scala
+++ src/main/scala/pass/CodeGenerator.scala
@@ -47,7 +47,16 @@ class CodeGenerator extends ExpressionVisitor[Unit, String] {
       varStack.push(CodeVariable(s"temp$tempCount", ty))
       val r = currentVar.code
       val arrayPostfix = if (ty.isInstanceOf[Type.Scalar]) { "" } else { "[64]" }
-      (r, s"${addressSpace.getOrElse(MemoryAllocator.PrivateMemory).toCL} ${ty.toCL} $r$arrayPostfix;")
+      (r, s"${addressSpace.getOrElse(MemoryAllocator.PrivateMemory).toCL} ${dereferenceTypeStr(ty.toCL)} $r$arrayPostfix;")
+    }
+  }
+
+  def dereferenceTypeStr(typeStr: String) = {
+    if (typeStr.endsWith("*")) {
+      typeStr.slice(0, typeStr.length - 1)
+    }
+    else {
+      typeStr
     }
   }
 
@@ -81,138 +90,124 @@ class CodeGenerator extends ExpressionVisitor[Unit, String] {
 
   override def visit(node: Expression.Apply, arg: ArgumentType): ResultType = {
     node.callee match {
-      case Expression.Identifier("mapSeq", false) => {
-        val func = node.args(0)
-        val (collection, prevCode) = safeAcceptAndPop(node.args.lift(1))
+      case Expression.Identifier(id, false) => {
+        id match {
+          case "mapSeq" => {
+            val func = node.args(0)
+            val (collection, prevCode) = safeAcceptAndPop(node.args.lift(1))
 
-        val Type.Array(inner, length) = node.callee.ty.asInstanceOf[Type.Arrow].nthArg(1) // collection.ty
+            val Type.Array(inner, length) = node.callee.ty.asInstanceOf[Type.Arrow].nthArg(1) // collection.ty
 
-        val resultType = node.ty
-        val (result, resultDecl) = generateResult(node.addressSpace, resultType)
+            val resultType = node.ty
+            val (result, resultDecl) = generateResult(node.addressSpace, resultType)
 
-        val vi = mkIndexVar
+            val vi = mkIndexVar
 
-        varStack.push(CodeVariable(s"${collection.code}[$vi]", inner))
-        val funcCode = func.accept(this, ())
-        val resultCode = varStack.pop().code
+            varStack.push(CodeVariable(s"${collection.code}[$vi]", inner))
+            val funcCode = func.accept(this, ())
+            val resultCode = varStack.pop().code
 
-        varStack.push(CodeVariable(result, resultType))
+            varStack.push(CodeVariable(result, resultType))
 
-        s"""
-           |// view = ${node.view}, code = ${ViewConstructor.construct(node.view)}
-           |$prevCode
-           |$resultDecl
-           |{
-           |  for (int $vi = 0; $vi < ${length.toCL}; $vi++) {
-           |    $funcCode
-           |    $result[$vi] = $resultCode;
-           |  }
-           |}
-         """.stripMargin
-      }
-      case Expression.Identifier("mapGlb", false) => {
-        /*
-        val Expression.Lambda(args, _) = node.args(0)
+            s"""
+               |// view = ${node.view}, code = ${ViewConstructor.construct(node.view)}
+               |$prevCode
+               |$resultDecl
+               |{
+               |  for (int $vi = 0; $vi < ${length.toCL}; $vi++) {
+               |    $funcCode
+               |    $result[$vi] = $resultCode;
+               |  }
+               |}
+            """.stripMargin
+          }
+          case "mapGlb" => {
+            val Expression.Lambda(args, _) = node.args(0)
 
-        var prevCode = ""
+            var prevCode = ""
 
-        val collectionName = node.args(1) match {
-          case Expression.Identifier(name, false) => name
-          case arg@_ => {
-            prevCode = arg.accept(this, ())
-            currentVar.code
+            val collectionName = node.args(1) match {
+              case Expression.Identifier(name, false) => name
+              case arg@_ => {
+                prevCode = arg.accept(this, ())
+                currentVar.code
+              }
+            }
+
+            val Type.Array(inner, _) = node.args(1).ty
+
+            val (result, resultDecl) = generateResult(node.addressSpace, inner)
+
+            s"""
+               |$prevCode
+               |$resultDecl
+               |{
+               |  int gid = get_global_id(0);
+               |  ${inner.toCL} ${args(0).value} = $collectionName[gid];
+               |  $result[gid] = ${node.args(0).accept(this, ())};
+               |}
+            """.stripMargin
+          }
+          case "reduceSeq" => {
+            val init = node.args(0)
+            val func = node.args(1)
+            var (collection, prevCode) = safeAcceptAndPop(node.args.lift(2))
+
+            prevCode += init.accept(this, ()) + "\n"
+            val initCode = varStack.pop()
+
+            val acc = CodeVariable("acc", init.ty)
+
+            varStack.push(acc)
+            varStack.push(CodeVariable(ViewConstructor.construct(node.view).right.get, init.ty))
+            val funcCode = func.accept(this, ())
+            val resultCode = varStack.pop().code
+
+            val Type.Array(inner, length) = node.callee.ty.asInstanceOf[Type.Arrow].nthArg(2)
+
+            val vi = mkIndexVar
+
+            varStack.push(acc)
+
+            s"""
+               |$prevCode
+               |${init.ty.toCL} ${acc.code} = $initCode;
+               |{
+               |  for (int $vi = 0; $vi < ${length.toCL}; $vi++) {
+               |    $funcCode
+               |    ${acc.code} = $resultCode;
+               |  }
+               |}
+            """.stripMargin
+          }
+          case "join" => {
+            // do nothing
+            ""
+          }
+          case "split" => {
+            // do nothing
+            ""
+          }
+          case "toGlobal" => node.args(0).accept(this, ())
+          case "toLocal" => node.args(0).accept(this, ())
+          case "toPrivate" => node.args(0).accept(this, ())
+          // case id@(Expression.Identifier("+", false) | Expression.Identifier("*", false)) => {
+          case op@("+" | "*") => {
+            val resultType = node.callee.ty.representativeType
+
+            val (left, prevLeft) = safeAcceptAndPop(node.args.lift(0))
+            val (right, prevRight) = safeAcceptAndPop(node.args.lift(1))
+
+            val (result, resultDecl) = generateResult(None, resultType)
+
+            s"""
+               |$resultDecl
+               |$prevLeft
+               |$prevRight
+               |$result = ${left.code} $op ${right.code};
+            """.stripMargin
           }
         }
-
-        val Type.Array(inner, _) = node.args(1).ty
-
-        val (result, resultDecl) = generateResult(node.addressSpace, inner)
-
-        s"""
-           |$prevCode
-           |$resultDecl
-           |{
-           |  int gid = get_global_id(0);
-           |  ${inner.toCL} ${args(0).value} = $collectionName[gid];
-           |  $result[gid] = ${node.args(0).accept(this, ())};
-           |}
-         """.stripMargin
-         */
-        ""
-      }
-      case Expression.Identifier("reduceSeq", false) => {
-        val init = node.args(0)
-        val func = node.args(1)
-        var (collection, prevCode) = safeAcceptAndPop(node.args.lift(2))
-
-        prevCode += init.accept(this, ()) + "\n"
-        val initCode = varStack.pop()
-
-        val acc = CodeVariable("acc", init.ty)
-
-        varStack.push(acc)
-        varStack.push(CodeVariable(ViewConstructor.construct(node.view).right.get, init.ty))
-        val funcCode = func.accept(this, ())
-        val resultCode = varStack.pop().code
-
-        // val (result, resultDecl) = generateResult(node.addressSpace, acc.ty)
-
-        val Type.Array(inner, length) = node.callee.ty.asInstanceOf[Type.Arrow].nthArg(2)
-
-        // val Expression.Lambda(args, body) = node.args(1)
-        // val Expression.Identifier(collectionName, _) = node.args(2)
-//        val Type.Array(inner, length) = node.args(2).ty
-//        val resultType = node.callee.ty.asInstanceOf[Type.Arrow].lastResultType
-//
-//
-//        val acc = args(0).value
-
-        val vi = mkIndexVar
-
-        varStack.push(acc)
-
-        s"""
-           |$prevCode
-           |${init.ty.toCL} ${acc.code} = $initCode;
-           |{
-           |  for (int $vi = 0; $vi < ${length.toCL}; $vi++) {
-           |    $funcCode
-           |    ${acc.code} = $resultCode;
-           |  }
-           |}
-         """.stripMargin
-      }
-      case Expression.Identifier("join", false) => {
-        // do nothing
-        ""
-      }
-      case Expression.Identifier("split", false) => {
-        // do nothing
-        ""
-      }
-      case Expression.Identifier("toGlobal", false) => node.args(0).accept(this, ())
-      case Expression.Identifier("toLocal", false) => node.args(0).accept(this, ())
-      case Expression.Identifier("toPrivate", false) => node.args(0).accept(this, ())
-      case Expression.Identifier("*", false) => {
-        s"""
-           |${node.args(0).accept(this, ())} * ${node.args(1).accept(this, ())}
-         """.stripMargin
-      }
-      case Expression.Identifier("+", false) => {
-        val resultType = node.callee.ty.representativeType
-
-        val (left, prevLeft) = safeAcceptAndPop(node.args.lift(0))
-        val (right, prevRight) = safeAcceptAndPop(node.args.lift(1))
-
-        val (result, resultDecl) = generateResult(None, resultType)
-
-        // varStack.push(CodeVariable(result, resultType))
-        s"""
-           |$resultDecl
-           |$prevLeft
-           |$prevRight
-           |$result = ${left.code} + ${right.code};
-         """.stripMargin
       }
     }
   }
