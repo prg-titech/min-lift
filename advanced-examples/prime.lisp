6afb9291f30666876e2dd14bd60e4c7303c8fdf5
long-long-float <niinikazuki@yahoo.co.jp>
2019-09-27 21:14:30 +0900

    Fix an example

--- advanced-examples/prime.lisp
+++ advanced-examples/prime.lisp
@@ -2,9 +2,7 @@
  (N)
  ((array-type int N) int)
  (lambda (xs n)
-  (unpack ys
-   ((toLocal (filterGlb (lambda (x)
+   (filterGlb (lambda (x)
                 (or
-                  (=i x n) (not (=i (mod x n) 0)))))) xs)
-   (pack ys))))
+                  (=i x n) (not (=i (mod x n) 0)))) xs)))
 
