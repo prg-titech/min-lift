da6e1e40456b20ad35aeae3d62f9215810797952
long-long-float <niinikazuki@yahoo.co.jp>
2019-12-15 23:41:41 +0900

    Fix euler30

--- advanced-examples/euler30.lisp
+++ advanced-examples/euler30.lisp
@@ -2,16 +2,18 @@
  (N)
  ((array-type int N))
  (lambda (xs)
-  (let fourthPower (lambda (x) (*i (*i x x)(*i x x)))
+  (let fourthPower (lambda (x) (*i (*i (*i x x)(*i x x)) x))
      (let ys
       ((toLocal (filterGlb (lambda (x)
                   (and
                     (not (=i x 1))
                     (let sumOfDidits
-                        (+i (fourthPower (/i x 1000))
+                        (+i (fourthPower (mod (/i x 100000) 10))
+                        (+i (fourthPower (mod (/i x 10000) 10))
+                        (+i (fourthPower (mod (/i x 1000) 10))
                         (+i (fourthPower (mod (/i x 100) 10))
                         (+i (fourthPower (mod (/i x 10)  10))
-                            (fourthPower (mod x 10)))))
+                            (fourthPower (mod x 10)))))))
                       (=i sumOfDidits x))
                     )
                   ))) xs)
